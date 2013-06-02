3bcdfa8ecf89b1c2e0be723e3feef35855d5034b
martin wellbourne <chris@floatingworks.co.uk>
2013-06-02 20:04:21 +0100

    shell script added

---
+++ nexuizname.sh
@@ -0,0 +1,15 @@
+#!/bin/bash  
+
+while true; do
+	if [ "$(pidof banshee)" ]; then
+        # sleep program for 2 minutes in production
+        # for testing I set this to 15 seconds
+		#sleep 2m 
+		sleep 15
+		UP=$(banshee --query-title)
+		GROUP=$(banshee --query-artist)
+		sed -i "s/seta songlist.*/seta songlist \"wellbourne (${GROUP} - ${UP})\""/ /home/chris/.nexuiz/data/namechange.cfg
+else
+		sed -i "s/seta songlist.*/seta songlist \"wellbourne (silent)\""/ /home/chris/.nexuiz/data/namechange.cfg
+	fi
+done

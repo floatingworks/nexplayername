db8638c852db471204bd475406d2ddd3c9477efb
martin wellbourne <chris@floatingworks.co.uk>
2013-06-03 09:04:08 +0100

    changed variable to TITLE

--- nexuizname.sh
+++ nexuizname.sh
@@ -1,14 +1,16 @@
 #!/bin/bash  
-
+# TODO
+# if the namechange.cfg file doesnt exist create it.
 while true; do
 	if [ "$(pidof banshee)" ]; then
-        # sleep program for 2 minutes in production
+        # sleep script for 1 minutes in production
         # for testing I set this to 15 seconds
-		#sleep 2m 
-		sleep 15
-		UP=$(banshee --query-title)
+		sleep 1m 
+		#sleep 15
+		TITLE=$(banshee --query-title)
 		GROUP=$(banshee --query-artist)
-		sed -i "s/seta songlist.*/seta songlist \"wellbourne (${GROUP} - ${UP})\""/ /home/chris/.nexuiz/data/namechange.cfg
+        echo playing ${GROUP} by ${UP}
+		sed -i "s/seta songlist.*/seta songlist \"wellbourne (${GROUP} - ${TITLE})\""/ /home/chris/.nexuiz/data/namechange.cfg
 else
 		sed -i "s/seta songlist.*/seta songlist \"wellbourne (silent)\""/ /home/chris/.nexuiz/data/namechange.cfg
 	fi

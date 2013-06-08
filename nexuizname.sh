25fdadaf5a87f175b284fd0641e87c3d225377d0
wellbourne <chris@floatingworks.co.uk>
2013-06-08 23:52:18 +0200

    Update nexuizname.sh

--- nexuizname.sh
+++ nexuizname.sh
@@ -2,10 +2,7 @@
 
 while true; do
 	if [ "$(pidof banshee)" ]; then
-        # sleep program for 2 minutes in production
-        # for testing I set this to 15 seconds
-		#sleep 2m 
-		sleep 15
+		sleep 40 
 		UP=$(banshee --query-title)
 		GROUP=$(banshee --query-artist)
 		sed -i "s/seta songlist.*/seta songlist \"wellbourne (${GROUP} - ${UP})\""/ /home/wellbourne/.nexuiz/data/namechange.cfg

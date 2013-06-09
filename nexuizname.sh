5541b7940fe59dfb197031b7bf8704759b100438
wellbourne <chris@floatingworks.co.uk>
2013-06-09 11:50:40 +0200

    Update nexuizname.sh
    moved instructions to readme

--- nexuizname.sh
+++ nexuizname.sh
@@ -1,16 +1,5 @@
 #!/bin/bash  
-#
-# Changes your nexuiz username dependent on Banshee media file
-# You will need to manually create the file ~/youruser/.nexuiz/data/namechange.cfg
-# You will need to put in your autoexec.cfg file the following two lines
-# 	alias update exec namechange.cfg
-# 	bind k update
-# Then run this script
-# and use the k key (or whatever you bound the alias update to) to update your username in game
-# any comments 
-#
-#TODO 	if namechange.cfg doesnt exist, create it
-#	use something like http://ubuntuforums.org/showthread.php?t=1562395 to make it non banshee specific	
+# Check readme for setup
 
 while true; do
 	if [ "$(pidof banshee)" ]; then

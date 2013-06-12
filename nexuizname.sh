1fe7cc2f116cb959fec33ee541073ab7e4856e14
martin wellbourne <chris@floatingworks.co.uk>
2013-06-12 10:08:16 +0100

    namechange.cfg file now includes all necessary script

--- nexuizname.sh
+++ nexuizname.sh
@@ -1,9 +1,15 @@
 #!/bin/bash  
 # Check readme for setup
-
+line1='seta songlist "wellbourne (silent)"'
+line2='name $songlist'
+line3='defer 30 "exec namechange2.cfg"'
 # if the namechange.cfg file doesnt exist create it.
 if [ ! -f ~/.nexuiz/data/namechange.cfg ]; then 
-    echo 'seta songlist "wellbourne (silent)" ' > ~/.nexuiz/data/namechange.cfg
+    cat  > ~/.nexuiz/data/namechange.cfg <<EOF
+${line1}
+${line2}
+${line3}
+EOF
 fi
 
 while true; do
